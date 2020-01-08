cd180ca92495fad277911cdce94feece4f075081
StarbrandedSentry <hierarchhero@gmail.com>
2020-01-09 03:02:58 +0800

    YEAH I WRITE FUCNTIONS HOW CAN YOU TELL :+1: :corn: :corn:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,33 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import FieldValue = admin.firestore.FieldValue;
+admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const incrementUsers = functions.firestore
-  .document('')
+  .document('invitations/{invitation}')
   .onWrite((change, context) => {
     //WRITE THE FUNCTIONS MICO
+    const afterData = change.after.data();
+
+    if (!afterData) {
+      return;
+    }
+
+    if (afterData.status === 'approved') {
+      if (afterData.invitationType === 1) {
+        admin
+          .firestore()
+          .doc('centers/' + afterData.centerID)
+          .update({ adminCount: FieldValue.increment(1) })
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    }
   });
--- src/app/firebase-collection/firebase-collection.module.ts
+++ src/app/firebase-collection/firebase-collection.module.ts
@@ -3,12 +3,14 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
 
 const Firebase = [
   AngularFirestoreModule,
   AngularFireStorageModule,
   AngularFireAuthModule,
-  AngularFireAuthGuardModule
+  AngularFireAuthGuardModule,
+  AngularFireFunctionsModule
 ];
 
 @NgModule({
