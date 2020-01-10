c1234d31fec680de9b2ecc0d1ea0b8f39e632625
sanlu0901 <sanluis.guigsy98@gmail.com>
2020-01-11 01:39:40 +0800

    downlaod url :globe_with_meridians:

--- src/app/center/center-research-add/center-research-add.component.ts
+++ src/app/center/center-research-add/center-research-add.component.ts
@@ -6,6 +6,7 @@ import {
 import { Observable } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-center-research-add',
@@ -14,16 +15,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class CenterResearchAddComponent implements OnInit {
   url: string;
-
   task: AngularFireUploadTask;
-
   percentage: Observable<number>;
-
   snapshot: Observable<any>;
-
   downloadURL: Observable<string>;
-
   isHovering: boolean;
+  researchFormGroup: FormGroup;
 
   constructor(
     private storage: AngularFireStorage,
@@ -33,10 +30,21 @@ export class CenterResearchAddComponent implements OnInit {
   startUpload(event: FileList) {
     const file = event.item(0);
     const path = 'test/' + new Date().getTime() + '_' + file.name;
+    const ref = this.storage.ref(path);
 
     this.task = this.storage.upload(path, file);
 
     this.percentage = this.task.percentageChanges();
+
+    this.task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = ref.getDownloadURL();
+          this.downloadURL.subscribe(url => (this.url = url));
+        })
+      )
+      .subscribe();
 
     this.snapshot = this.task.snapshotChanges().pipe(
       tap(snap => {
