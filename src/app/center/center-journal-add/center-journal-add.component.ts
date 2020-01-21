0962c8ba7b7f05465b763e0f210865aebe603537
sanlu0901 <sanluis.guigsy98@gmail.com>
2020-01-22 01:26:29 +0800

    add journal works :squirrel:

--- src/app/center/center-journal-add/center-journal-add.component.ts
+++ src/app/center/center-journal-add/center-journal-add.component.ts
@@ -3,6 +3,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/shared/category.service';
+import { Journal } from 'src/app/models/journal.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Center } from 'src/app/models/center.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { JournalService } from 'src/app/shared/journal.service';
 
 @Component({
   selector: 'app-center-journal-add',
@@ -11,12 +17,16 @@ import { CategoryService } from 'src/app/shared/category.service';
 })
 export class CenterJournalAddComponent implements OnInit {
   journalFormGroup: FormGroup;
+  center$: Observable<Center>;
+  center: Center;
   centerID: string;
 
   constructor(
     public categoryService: CategoryService,
+    private journalService: JournalService,
     private formBuilder: FormBuilder,
-    private ar: ActivatedRoute
+    private ar: ActivatedRoute,
+    private afFirestore: AngularFirestore
   ) {}
 
   get journalTitle() {
@@ -40,8 +50,22 @@ export class CenterJournalAddComponent implements OnInit {
   }
 
   ngOnInit() {
+    // Get center ID through the params
     this.ar.parent.parent.paramMap.subscribe(params => {
       this.centerID = params.get('id');
+      this.center$ = this.afFirestore
+        .doc('centers/' + this.centerID)
+        .snapshotChanges()
+        .pipe(
+          map(a => {
+            const data = a.payload.data() as Center;
+            data.id = a.payload.id;
+            return data;
+          })
+        );
+    });
+    this.center$.subscribe(center => {
+      this.center = center;
     });
 
     this.journalFormGroup = this.formBuilder.group({
@@ -51,5 +75,18 @@ export class CenterJournalAddComponent implements OnInit {
       category: ['', [Validators.minLength(8), Validators.required]],
       description: ['', [Validators.minLength(8), Validators.required]]
     });
+  }
+
+  onAddJournalClick(): void {
+    const newJournal: Journal = {
+      title: this.journalTitle.value,
+      centerID: this.centerID,
+      centerName: this.center.name,
+      publisher: this.publisher.value,
+      date: this.publishDate.value,
+      category: this.category.value,
+      description: this.description.value
+    };
+    this.journalService.addNewJournal(newJournal);
   }
 }
---
+++ src/app/models/journal.model.ts
@@ -0,0 +1,10 @@
+export interface Journal {
+    id?: string;
+    title?: string;
+    centerID?: string;
+    centerName?: string;
+    publisher?: string;
+    date?: Date;
+    category?: string;
+    description?: string;
+}
